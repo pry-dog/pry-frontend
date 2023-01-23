b209a93d7a0407080c964f7aa79364412a7791b5
parabirb <parabirb@protonmail.ch>
2023-01-23 15:58:54 -0600

    push svelte skeleton

---
+++ README.md
@@ -0,0 +1,8 @@
+# pry-frontend
+the frontend to pry.dog. open source for transparency.
+
+## licensing
+pry-frontend is licensed under the AGPLv3. please use it accordingly.
+
+## documentation
+none will be provided.
---
+++ tailwind.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+    content: ["./src/**/*.{html,svelte}"],
+    theme: {
+        extend: {},
+        fontFamily: {
+            sans: ["Inter", "sans-serif"]
+        }
+    },
+    plugins: []
+};
